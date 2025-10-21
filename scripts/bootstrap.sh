acd966e5287bd24b78daa54c660cdc7ab014cfb9
Landrey Smith <smithlandrey@gmail.com>
2025-10-20 19:07:33 -0700

    update bootstrap script to fix quickstart.

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -1,7 +1,16 @@
 #!/usr/bin/env bash
 set -euo pipefail
+
 pnpm install
+
 pushd apps/api
+    pnpm install
     pnpm prisma migrate deploy
 popd
+
+pushd apps/web
+    pnpm install
+popd
+
 echo "Bootstrap complete"
+echo "Run 'pnpm dev:api' and 'pnpm dev:web' to start the dev servers."
